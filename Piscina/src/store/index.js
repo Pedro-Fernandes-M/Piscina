0c9c37a624ab2ed64eaa2a1ab3ee4a3ee18222e4
IoAcademy - Pedro Fernandes <pedrofernandeslegsi@outlook.pt>
2025-06-18 20:08:04 +0100

    modified:   Piscina/.gitignore 	modified:   Piscina/package.json 	modified:   Piscina/src/assets/main.css 	modified:   Piscina/src/store/index.js 	modified:   Piscina/src/views/TheForm.vue

--- Piscina/src/store/index.js
+++ Piscina/src/store/index.js
@@ -1,61 +1,26 @@
 import { createStore } from 'vuex'
 
-let googleCredentialResolve = null;
+
 const store= createStore({
     state:{
-        count:1,
         googleCredential: null,
     },
     getters:{
-        getCount(state){
-            return state.count
-        },
-        getAuth(state){
+        getGoogleCredential(state){
             return state.googleCredential
         }
     },
     mutations:{
-        SetGoogleCredential(state, token) {
+        setGoogleCredential(state, token) {
             state.googleCredential = token;
         },
     },
     actions:{
-       initGoogleClient({ commit }) {
-      return new Promise((resolve) => {
-        if (window.google && window.google.accounts) {
-          window.google.accounts.id.initialize({
-            client_id: '145201799411-o6s35c72gu25nq53m4pmhq5focl09uf1.apps.googleusercontent.com',
-            callback: (response) => {
-              console.log(response.credential)
-              commit('SetGoogleCredential', response.credential);
-              if (googleCredentialResolve) googleCredentialResolve(response.credential);
-            },
-          });
-
-          resolve(); // já inicializado
-        } else {
-          console.error('Google Identity Services not loaded');
-        }
-      });
-    },
-
-    async signIn({ dispatch }) {
-    await dispatch('initGoogleClient');
-        return await new Promise((resolve, reject) => {
-            googleCredentialResolve = resolve;
-
-            window.google.accounts.id.prompt((notification) => {
-                if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
-                    reject('Google sign-in was cancelled or blocked');
-                }
-            });
-        });
-    },
-
         solicitarToken() {
         return new Promise((resolve, reject) => {
+            console.log(import.meta.env.VITE_CLIENT_ID)
             const tokenClient = window.google.accounts.oauth2.initTokenClient({
-            client_id: '145201799411-o6s35c72gu25nq53m4pmhq5focl09uf1.apps.googleusercontent.com',
+            client_id: import.meta.env.VITE_CLIENT_ID,
             scope: 'https://www.googleapis.com/auth/spreadsheets',
             callback: (tokenResponse) => {
                 if (tokenResponse.error) {
@@ -70,11 +35,12 @@ const store= createStore({
         });
         },
 
-        async preencherSheet({dispatch}) {
-        // 1. Pede o token OAuth
-        const token = await dispatch('solicitarToken');
-        const apiKey= 'AIzaSyAStEn26qXeXWIIc-LYglVzMh8iqaJTKL4';
-        const spreadsheetId= '1Uthc13UmWYSp0A3VFCGDvDejd3n5LK9k9y9l09UQKeQ';
+        async preencherSheet({getters,dispatch,commit},payload) {
+        if(getters.getGoogleCredential==null){
+            commit('setGoogleCredential',await dispatch('solicitarToken')) ;
+        }
+        const apiKey= import.meta.env.VITE_API_KEY;
+        const spreadsheetId= import.meta.env.VITE_SPREADSHEET_ID;
 
         // 2. Carrega e inicializa gapi client
         await new Promise((resolve) => window.gapi.load('client', resolve));
@@ -84,11 +50,30 @@ const store= createStore({
         });
 
         // 3. Seta o token no client
-        window.gapi.client.setToken({ access_token: token });
+        window.gapi.client.setToken({ access_token: getters.getGoogleCredential });
 
+          try {
+            // 4. Lê a coluna A (onde está o "Dia")
+            const readResponse = await window.gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId,
+            range: `${payload.mes}!A:A`,
+            });
+
+            const dias = readResponse.result.values || [];
+
+            // 5. Procura a linha onde o valor da coluna A bate com o diaAlvo
+            const linhaIndex = dias.findIndex(row => row[0] == String(payload.dia));
+
+            if (linhaIndex === -1) {
+            console.error('Dia não encontrado na coluna A');
+            return;
+            }
+            
+        const linha = linhaIndex + 1;
         // 4. Prepara dados e chama API
-        const range = "Janeiro!A1:B2";
-        const values = [['Nome', 'Email'], ['João', 'joao@email.com']];
+        const range = payload.mes+"!B"+linha+":F"+linha; // Linha 1, colunas A, B e C
+        const values = [[payload.horas.value, payload.num_pessoas.value, payload.ph_cima.value, payload.ph_baixo.value,'Filipe Fernandes']];
+
 
         const params = {
             spreadsheetId,
@@ -105,13 +90,19 @@ const store= createStore({
             params,
             resource
             );
+            alert('Planilha atualizada com sucesso')
             console.log('Planilha atualizada com sucesso:', response);
             return response;
         } catch (error) {
+            alert('Erro ao atualizar a planilha')
             console.error('Erro ao atualizar a planilha:', error);
             throw error;
         }
+        }catch (error) {
+            console.error('Erro ao atualizar por dia:', error);
+            throw error;
         }
+    }
     },
     modules:{}
 })
--- Piscina/src/views/TheForm.vue
+++ Piscina/src/views/TheForm.vue
@@ -1,28 +1,149 @@
 <template>
-    <div>
-        <button @click="handleLogin">Login com Google</button>
-        <button @click="store.dispatch('preencherSheet')">Preencher</button>
+    <div class="page">
+        <form action="">
+          <div class="center">
+            <h2>
+              Piscina
+            </h2>
+            <h3>{{ day }} de {{ month }}</h3>
+          </div>
+          <div class="input">
+            <label for="ph">PH_cima</label>
+            <input :class="[ ph_cima===  '' ? 'input-erro': '']" type="number" v-model="ph_cima">
+          </div>
+          <div class="input">
+            <label for="ph">PH_baixo</label>
+            <input :class="[ ph_baixo=== '' ? 'input-erro': '']" type="number" v-model="ph_baixo">
+          </div>
+          <div class="input">
+            <label for="num">Nº Pessoas</label>
+            <input :class="[ num_pessoas=== '' ? 'input-erro': '']" type="number" v-model="num_pessoas">
+          </div>
+          <button @click.prevent="preencher" type="submit" >Preencher</button>
+        </form>
 
     </div>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore()
+const hoje = new Date();
+const day = hoje.getDate();
+const monthNames = [
+  'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+  'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+];
+const month = monthNames[hoje.getMonth()];
 
-async function handleLogin() {
-      try {
-        await store.dispatch('signIn');
-        alert('Autenticado com sucesso!');
-      } catch (err) {
-        console.error('Erro ao autenticar', err);
-      }
+const ph_cima= ref(null)
+const ph_baixo= ref(null)
+const num_pessoas= ref(0)
+const horas = ref(null)
+
+const validade= computed(()=>{
+  const n = Number(num_pessoas.value);
+  const ph_c= Number(ph_cima.value)
+  const ph_b= Number(ph_baixo.value)
+  return !isNaN(n) && num_pessoas.value!=null && n >= 0 && Number.isInteger(n)  && !isNaN(n) && ph_cima.value!=null && ph_c >= 0 && Number.isInteger(ph_c) && !isNaN(n) && ph_baixo.value!=null && ph_b >= 0 && Number.isInteger(ph_b);
+})
+
+
+function preencher(){
+  let hours = hoje.getHours();
+  let minutes = hoje.getMinutes();
+  hours = String(hours).padStart(2, '0');
+  minutes = String(minutes).padStart(2, '0');
+  horas.value= hours+':'+minutes
+
+  if(validade.value && horas.value!=null && month!=null && day!=null){
+  const form={
+    ph_cima:ph_cima,
+    ph_baixo:ph_baixo,
+    num_pessoas: num_pessoas,
+    horas:horas,
+    mes: month,
+    dia:day
+  }
+
+  store.dispatch('preencherSheet',form)
+  }
+  else{
+    alert('Formulário mal preenchido!')
+  }
 }
-
-
 </script>
 
 <style scoped>
+.page{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100dvh;
+}
 
+
+form{
+  background-color: #3a3a3a;
+  padding: 30px 40px;
+  border-radius: 12px;
+  box-shadow: 0 4px 12px rgba(0,0,0,0.3);
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 300px;
+}
+
+.center{
+  text-align: center;
+}
+
+h2{
+  color: aliceblue;
+}
+
+.input{
+  display: grid;
+  margin-top: 0.5rem;
+  padding-bottom: 1rem;
+}
+
+input {
+  padding: 10px 15px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  font-size: 16px;
+  background-color: #444;
+  color: #fff;
+  width: 200px;
+  transition: box-shadow 0.2s ease;
+  width: 100%;
+}
+
+input:focus {
+  box-shadow: 0 0 0 2px #00bcd4;
+}
+
+.input-erro{
+  box-shadow: 0 0 0 2px #d42e00;
+}
+
+button {
+  margin-top: 1rem;
+  padding: 10px 20px;
+  background-color: #00bcd4;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background 0.2s ease;
+}
+
+button:hover {
+  background-color: #0097a7;
+}
 </style>
