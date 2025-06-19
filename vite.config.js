d022301c19524f484077a7f500951459248b209d
IoAcademy - Pedro Fernandes <pedrofernandeslegsi@outlook.pt>
2025-06-19 01:13:08 +0100

    renamed:    Piscina/.editorconfig -> .editorconfig 	renamed:    Piscina/.gitattributes -> .gitattributes 	renamed:    Piscina/.github/workflows/deploy.yaml -> .github/workflows/deploy.yaml 	renamed:    Piscina/.gitignore -> .gitignore 	renamed:    Piscina/.prettierrc.json -> .prettierrc.json 	renamed:    Piscina/.vscode/extensions.json -> .vscode/extensions.json 	renamed:    Piscina/README.md -> README.md 	renamed:    Piscina/eslint.config.js -> eslint.config.js 	renamed:    Piscina/index.html -> index.html 	renamed:    Piscina/jsconfig.json -> jsconfig.json 	renamed:    Piscina/localhost-key.pem -> localhost-key.pem 	renamed:    Piscina/localhost.pem -> localhost.pem 	renamed:    Piscina/package-lock.json -> package-lock.json 	renamed:    Piscina/package.json -> package.json 	renamed:    Piscina/public/image.ico -> public/image.ico 	renamed:    Piscina/public/maifest.json -> public/maifest.json 	renamed:    Piscina/src/App.vue -> src/App.vue 	renamed:    Piscina/src/assets/base.css -> src/assets/base.css 	renamed:    Piscina/src/assets/logo.svg -> src/assets/logo.svg 	renamed:    Piscina/src/assets/main.css -> src/assets/main.css 	renamed:    Piscina/src/components/icons/IconDocumentation.vue -> src/components/icons/IconDocumentation.vue 	renamed:    Piscina/src/main.js -> src/main.js 	renamed:    Piscina/src/router/index.js -> src/router/index.js 	renamed:    Piscina/src/store/index.js -> src/store/index.js 	renamed:    Piscina/src/views/TheForm.vue -> src/views/TheForm.vue 	renamed:    Piscina/vite.config.js -> vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import fs from 'fs';
+//import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
