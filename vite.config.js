8b8b33f69e91c1e5b5b5b5cbc4352574406abe4d
IoAcademy - Pedro Fernandes <pedrofernandeslegsi@outlook.pt>
2025-06-19 01:20:01 +0100

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-/*   server: {
+  base: '/Piscina/',
+  /*   server: {
     https: {
       key: fs.readFileSync('./localhost-key.pem'),
       cert: fs.readFileSync('./localhost.pem'),
