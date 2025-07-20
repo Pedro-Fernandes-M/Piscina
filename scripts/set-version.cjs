bb0683b49e8de47170623431a3c14b28134e94e1
IoAcademy - Pedro Fernandes <pedrofernandeslegsi@outlook.pt>
2025-07-20 23:51:37 +0100

    modified:   scripts/set-version.cjs
    	modified:   .github/workflows/deploy.yaml

--- scripts/set-version.cjs
+++ scripts/set-version.cjs
@@ -2,6 +2,4 @@ const { execSync } = require('child_process')
 const fs = require('fs')
 
 const commitCount = execSync('git rev-list --count HEAD').toString().trim()
-const version = `0.0.${commitCount}`
-
-fs.writeFileSync('.env', `VITE_APP_VERSION=${version}\n`, { flag: 'a' }) // adiciona ao .env
+const version = commitCount > 50 ? `0.1.${commitCount}` : `0.0.${commitCount}`
