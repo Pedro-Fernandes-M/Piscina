4706761e7b00f6706aa3a60cbf54c2b7bafa66df
IoAcademy - Pedro Fernandes <pedrofernandeslegsi@outlook.pt>
2025-07-20 23:55:33 +0100

    modified:   scripts/set-version.cjs

--- scripts/set-version.cjs
+++ scripts/set-version.cjs
@@ -3,3 +3,5 @@ const fs = require('fs')
 
 const commitCount = execSync('git rev-list --count HEAD').toString().trim()
 const version = commitCount > 50 ? `0.1.${commitCount}` : `0.0.${commitCount}`
+
+fs.writeFileSync('.env', `VITE_APP_VERSION=${version}\n`)
