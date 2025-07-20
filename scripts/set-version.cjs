464b0ee9b6ae04c42febfba91cc76281d130954f
IoAcademy - Pedro Fernandes <pedrofernandeslegsi@outlook.pt>
2025-07-20 23:39:10 +0100

    modified:   scripts/set-version.cjs 	modified:   src/views/ChangeSettings.vue

--- scripts/set-version.cjs
+++ scripts/set-version.cjs
@@ -5,4 +5,3 @@ const commitCount = execSync('git rev-list --count HEAD').toString().trim()
 const version = `0.0.${commitCount}`
 
 fs.writeFileSync('.env', `VITE_APP_VERSION=${version}\n`, { flag: 'a' }) // adiciona ao .env
-console.log(`Versão gerada: ${version}`)
